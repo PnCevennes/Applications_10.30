3c469dbc61beab93ebb6c930efc6a8a6bcc7e73d
amandine-sahl <amandine.sahl@gmail.com>
2017-02-17 16:18:03 +0100

    suppression de la flore pour en faire un dépot spécifique

--- Sources/Flore/Modeles/Classes/ClassTaxLic.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once '../../Configuration/PostGreSQL.php';
-    require_once 'ClassTax.php';
-
-    class TaxLic extends Tax {
-        static protected $tableTax = 'saisie.taxon_lichen';
-    }
-?>
--- Sources/Flore/Securite/VerifCnx.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require_once '../Configuration/PostGreSQL.php';
-    
-    if (!(isset($_SESSION[APPLI]['numerisateur']['code']) && isset($_SESSION[APPLI]['numerisateur']['libelle']))) {
-        header('Location: vAuthent.php');
-    }
-?>
